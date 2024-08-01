81b0053a85a10ad867ecbe475a1254afeef8764e
Derrick Ennals <ilyki4822@gmail.com>
2024-08-01 19:55:03 -0400

    Bigger Boy

--- podio_utilis.py
+++ podio_utilis.py
@@ -1,5 +1,5 @@
 from pypodio2 import api 
-
+from urllib.parse import urlencode
 def authenticate_podio(client_id, client_secret, username, password):
     client = api.OAuthClient(client_id, client_secret, username, password)
     return client
